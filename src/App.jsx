bc79cc2b3937bf64d3b153c68822e8996e441fd2
125FARAONA <108438504+125FARAONA@users.noreply.github.com>
2026-02-06 23:02:03 -0400

    Update YouTube iframe source to embed format

--- src/App.jsx
+++ src/App.jsx
@@ -204,7 +204,7 @@ function Experiencia() {
       <iframe
         width="360"
         height="215"
-        src="https://www.youtube.com/watch?v=2UpwTp-owL4"
+        src="https://www.youtube.com/embed/2UpwTp-owL4?si=8X1FRrCqay9h2qyt"
         allowFullScreen
         style={{ borderRadius: "8px" }}
       ></iframe>
